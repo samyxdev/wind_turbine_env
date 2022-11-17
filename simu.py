d8e144c0270fb7ba38f3e41bea6ad51076720cfd
Samyxdev <samyxdev@proton.me>
2022-11-17 10:59:10 +0100

    Adding new agents and step function in GymAI format

--- simu.py
+++ simu.py
@@ -9,7 +9,18 @@
 import numpy as np
 from math_utils import wrap_to_m180_p180
 from wind_turbine import Wind_turbine, Wind
+import random as rd
 
+STEP_PER_DEG = 1 # -> Corresponds to a disc of 0.333
+
+def rounding_func(angle):
+    return round(angle*STEP_PER_DEG)/STEP_PER_DEG
+
+def angle_to_index(angle):
+    return round(angle*STEP_PER_DEG)
+
+def relangle_to_index(angle):
+	return int(round((angle+179)*STEP_PER_DEG)/STEP_PER_DEG)
 
 class Basic_agent:
 	__threshold = 5 					# deg, corresponds to the wind deadzone in which no action is taken
@@ -34,6 +45,23 @@ class Basic_agent:
 	def __str__(self):
 		return str(self.__class__) + ": " + str(self.__dict__)
 
+class Tracking_agent:
+	"""Custom agent designed to continually track and adjust rotation based on wind orientation
+	"""
+	def __init__(self):
+		pass
+
+	def policy(self, rel_wind_heading) -> int:
+		'''
+		Define the policy of the agent
+		Input  : relative wind heading between the wind and the wind turbine.
+		Ouptut : an int corresponding to the selected action : 0 rotate clockwise, 1 do nothgin, 2 rotate trigo
+		'''
+		rel_wind_heading = wrap_to_m180_p180(rel_wind_heading)
+		return np.sign(rel_wind_heading) + 1
+
+	def __str__(self):
+		return str(self.__class__) + ": " + str(self.__dict__)
 
 class Random_agent:
 	def __init__(self):
@@ -50,6 +78,17 @@ class Random_agent:
 		return str(self.__class__) + ": " + str(self.__dict__)
 
 
+class Epsgreedy_agent:
+	def __init__(self):
+		pass
+
+	def policy(self, rel_wind_heading, Q, eps) -> int:
+		return np.argmax(Q[relangle_to_index(rel_wind_heading)]) if rd.random() > eps else rd.choice([0, 1, 2])
+
+	def __str__(self):
+		return str(self.__class__) + ": " + str(self.__dict__)
+
+
 # An agent to be completed
 class Custom_agent:
 	def __init__(self):
@@ -57,6 +96,23 @@ class Custom_agent:
 
 	def policy(self, rel_wind_heading) -> int:
 		pass
+
+	def __str__(self):
+		return str(self.__class__) + ": " + str(self.__dict__)
+
+class Mapping_agent:
+	"""Agent that can be initialized with a fixed mapping state to action (= a policy).
+	If not specified, the initial policy is a random one."""
+	policy_map = None
+
+	def __init__(self, mapping=None):
+		if mapping == None:
+			self.policy_map = [rd.choice([0, 1, 2]) for _ in range(360*STEP_PER_DEG)]
+		else:
+			self.policy_map = mapping
+
+	def policy(self, rel_wind_heading) -> int:
+		return self.policy_map[angle_to_index(rel_wind_heading)]
 
 	def __str__(self):
 		return str(self.__class__) + ": " + str(self.__dict__)
@@ -98,10 +154,48 @@ class Simu:
 		# Generate new wind
 		self.wd.step()
 
+	def step_enhanced(self, action):
+		"""Step function returning the new state and the reward resulting from taking
+		the action passed in argument to this function."""
+		# Log the estimated wind (logging St)
+		self.rel_wind_heading_log[self.step_count] = wrap_to_m180_p180(self.wd.heading - self.wt.heading)
+
+		# Log the true wind
+		self.true_rel_wind_heading_log[self.step_count] = wrap_to_m180_p180(self.wd.heading - self.wt.true_heading)
+		self.wd_heading_log[self.step_count] = self.wd.heading
+
+		# Get action (logging At)
+		self.action_log[self.step_count] = action
+
+		# Apply action and get power output (Logging Rt+1)
+		self.power_output_log[self.step_count] = self.wt.step(self.wd.speed, self.wd.heading, self.action_log[self.step_count])
+
+		# Generate new wind
+		self.wd.step()
+
+		# GymAI step function return format: St+1, Rt+1
+		return wrap_to_m180_p180(self.wd.heading - self.wt.heading), self.power_output_log[self.step_count]
+
+	
+	def manual_step(self, action):
+		"""Manually apply a step of the environnment
+		and returns new state and reward"""
+		if self.step_count < self.max_steps:
+			stp, rt = self.step_enhanced(action)
+			
+			self.step_count += 1
+
+			return stp, rt, self.step_count >= self.max_steps
+		else:
+			raise Exception("Max steps reached")
+
 	def run_simu(self):
 		while self.step_count < self.max_steps:
 			self.step()
 			self.step_count += 1
+	
+	def get_initial_state(self):
+		return wrap_to_m180_p180(self.wd.heading - self.wt.heading)
 
 	def __str__(self):
 		return str(self.__class__) + ": " + str(self.__dict__)
